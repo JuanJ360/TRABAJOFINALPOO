46bf0c467c4dcbf93cba7b9dc49be9c116498228
J-p-M77 <131775741+J-p-M77@users.noreply.github.com>
2025-05-08 14:00:10 -0500

    c

---
+++ under_dark_dungeons/src/Tienda.h
@@ -0,0 +1,22 @@
+#ifndef TIENDA_H
+#define TIENDA_H
+
+#include <vector>
+#include "Objeto.h"
+#include "Booster_armas.h"
+#include "Booster_armaduras.h"
+
+using namespace std;
+
+class Tienda {
+private:
+    vector<Objeto*> objetos;
+    Booster_armas booster_armas;
+    Booster_armaduras booster_armaduras;
+
+public:
+    Tienda();
+    void seleccionar();
+};
+
+#endif
